3aae7d29554ebb60254e3105b934cb14576bd8e3
farhansadik007 <sadik.zisan@gmail.com>
2023-06-11 20:28:53 +0600

    verifyAdmin and verifyInstructor api Added

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization) return res.status(401).send({error: true, message: 'unauthorized access'});
+
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) return res.status(401).send({error: true, message: 'unauthorized access'});
+
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,8 +47,35 @@ async function run() {
         const usersCollection = client.db('dwDB').collection('users');
 
 
+        app.post('/jwt', (req, res)=> {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '6h'});
+
+            res.send({token});
+        })
+
+        const verifyAdmin = async(req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email}
+            const user = await usersCollection.findOne(query);
+            if(user?.role !== 'admin') {
+                return res.status(403).send({error: true, message: 'forbidden access'});
+            }
+            next();
+        }
+
+        const verifyInstructor = async(req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email}
+            const user = await usersCollection.findOne(query);
+            if(user?.role !== 'instructor') {
+                return res.status(403).send({error: true, message: 'forbidden access'});
+            }
+            next();
+        }
+
         //users
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -44,6 +86,28 @@ async function run() {
             const existingUser = await usersCollection.findOne(query);
             if (existingUser) return res.send({ message: 'user already exists' })
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/users/admin/:email', verifyJWT, async(req, res) => {
+            const email = req.params.email;
+
+            if(req.decoded.email !== email) res.send({admin:false});
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = {admin: user?.role === 'admin'};
+            res.send(result);
+        })
+
+        app.get('/users/instructor/:email', verifyJWT, async(req, res) => {
+            const email = req.params.email;
+
+            if(req.decoded.email !== email) res.send({instructor:false});
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = {instructor: user?.role === 'instructor'};
             res.send(result);
         })
 
@@ -86,11 +150,15 @@ async function run() {
 
 
         //carts
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([]);
             }
+
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail) return res.status(403).send({error: true, message: 'forbidden access'});
+
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result);
