1371c674cad6e2671acc5d27ba2661be846770de
farhansadik007 <sadik.zisan@gmail.com>
2023-06-11 16:46:49 +0600

    manage user role api added

--- index.js
+++ index.js
@@ -47,6 +47,30 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         //instructors
         app.get('/instructors', async (req, res) => {
             const result = await instructorsCollection.find().toArray();
